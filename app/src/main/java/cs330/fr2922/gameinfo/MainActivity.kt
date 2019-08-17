300dfdcb20a07315176f063d628b57e11891e286
FR97 <filip.ristic.2922@gmail.com>
2019-08-17 18:01:54 +0200

    Initial Commit

---
+++ app/src/main/java/cs330/fr2922/gameinfo/MainActivity.kt
@@ -0,0 +1,12 @@
+package cs330.fr2922.gameinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
